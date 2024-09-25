7a837fa5f8a53ba163965c5a921a4fcd7af51c43
Arun silawat <arunsilawat722362@gmail.com>
2024-09-25 17:27:03 +0530

    home page

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 urlpatterns = [
-    path('', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('', views.watch, name='watch'),
+    path('about/', views.about, name='about'),
+    path('new/', views.new, name='new'),
+    path('service/', views.service, name='service'),
+    path('sports/', views.sports, name='sports')
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def watch(request):
+    return render(request,'watch.html')
+def about(request):
+    return render(request,'about.html')
+def new(request):
+    return render(request,'new.html')
+def service(request):
+    return render(request,'service.html')
+def sports(request):
+    return render(request,'sports.html')
